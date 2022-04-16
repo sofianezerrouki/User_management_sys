a00b66f419273c94fc32ca13179242e6737bf29f
s.zerrouki <s.zerrouki@teletic.dz>
2022-04-16 18:02:41 +0100

    add security configuration to allow just authenticated and signUp requests

--- src/main/java/com/jobtest/me/user_management_account/config/JobUserDetailService.java
+++ src/main/java/com/jobtest/me/user_management_account/config/JobUserDetailService.java
@@ -3,12 +3,9 @@ package com.jobtest.me.user_management_account.config;
 import com.jobtest.me.user_management_account.models.User;
 import com.jobtest.me.user_management_account.repo.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
--- src/main/java/com/jobtest/me/user_management_account/config/ProjectConfig.java
+++ src/main/java/com/jobtest/me/user_management_account/config/ProjectConfig.java
@@ -1,25 +1,54 @@
 package com.jobtest.me.user_management_account.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JobUserDetailService jobUserDetailService;
+    @Autowired
+    private MyPasswordEncoder myPasswordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
+                .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/api/v1/register")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().formLogin();
 
-        http.csrf().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(myPasswordEncoder);
+        provider.setUserDetailsService(jobUserDetailService);
+        return provider;
+    }
+
 }
